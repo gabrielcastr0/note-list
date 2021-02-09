28abebe0a663fa12204d5b60f6be34a6123ed594
Gabriel Castro <gabrielcastro.xy@gmail.com>
2021-02-09 17:17:42 -0300

    Tela de Ediçao (2/5)

--- src/pages/EditNoteScreen/index.js
+++ src/pages/EditNoteScreen/index.js
@@ -1,11 +1,25 @@
-import React, {useState} from 'react';
-import {useNavigation} from '@react-navigation/native';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import {Container, TitleInput, BodyInput} from './styles';
-import {set} from 'react-native-reanimated';
 
 export default () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const dispatch = useDispatch();
+  const list = useSelector((state) => state.notes.list);
+
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const [status, setStatus] = useState('new');
+
+  useEffect(() => {
+    if (route.params?.key != undefined && list[route.params.key]) {
+      setStatus('edit');
+      setTitle(list[route.params.key].body);
+      setBody(list[route.params.key].body);
+    }
+  }, []);
 
   return (
     <Container>
